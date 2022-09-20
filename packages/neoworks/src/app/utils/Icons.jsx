1a5fe33900f28cd07d40968561839dc74b268d9e
Jay Parisi <jay.parisi@coinbase.com>
2022-09-20 11:58:41 -0400

    changed trade pair reference

--- packages/neoworks/src/app/utils/Icons.jsx
+++ packages/neoworks/src/app/utils/Icons.jsx
@@ -13,17 +13,17 @@ export const Icons = ({ asset }) => {
   switch (asset) {
     case 'BTC':
       return <Bitcoin height="45px" width="45px" />;
-    case 'BTC-USD':
+    case 'BTC_USD':
       return <Bitcoin height="45px" width="45px" />;
     case 'ETH':
       return <Ethereum height="45px" width="45px" />;
-    case 'ETH-USD':
+    case 'ETH_USD':
       return <Ethereum height="45px" width="45px" />;
     case 'Ethereum':
       return <Ethereum height="45px" width="45px" />;
     case 'SOL':
       return <Solana height="45px" width="45px" />;
-    case 'SOL-USD':
+    case 'SOL_USD':
       return <Solana height="45px" width="45px" />;
     case 'Solana':
       return <Solana height="45px" width="45px" />;
@@ -35,10 +35,15 @@ export const Icons = ({ asset }) => {
       return <Cosmos height="45px" width="45px" />;
     case 'MATIC':
       return <Polygon height="45px" width="45px" />;
-    case 'CARDANO':
+    case 'ADA_USD':
       return <Cardano height="45px" width="45px" />;
+    case 'ADA':
+      return <Cardano height="45px" width="45px" />;
+    case 'ATOM_USD':
+      return <Cosmos height="45px" width="45px" />;
     case 'ATOM':
       return <Cosmos height="45px" width="45px" />;
+
     case 'USD':
       return <img src={USD} alt="USD" />;
     default:
