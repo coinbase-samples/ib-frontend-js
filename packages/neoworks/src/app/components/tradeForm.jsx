b5feef7a34b1c45a29b7b4ab01ad1f02d0140b3a
Jay Parisi <jay.parisi@coinbase.com>
2022-10-03 16:08:40 -0400

    fix limit price load in text box

--- packages/neoworks/src/app/components/tradeForm.jsx
+++ packages/neoworks/src/app/components/tradeForm.jsx
@@ -184,7 +184,7 @@ export function TradeForm(props) {
               <FormField label="Limit Price" id="limitPrice" errorText={error}>
                 <Input
                   onChange={({ detail }) => handleLimitPrice(detail.value)}
-                  value={limitPrice}
+                  value={assetPrice}
                 />
               </FormField>
             ) : (
