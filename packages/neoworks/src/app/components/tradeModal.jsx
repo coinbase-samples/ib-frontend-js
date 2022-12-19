008aa4ddf9d060402b43e4ee7390a85dc185b109
Jay Parisi <jay.parisi@coinbase.com>
2022-12-19 17:15:27 -0500

    fix order modal load on trades condition

--- packages/neoworks/src/app/components/tradeModal.jsx
+++ packages/neoworks/src/app/components/tradeModal.jsx
@@ -98,7 +98,7 @@ export function TradeModal(props) {
   const overBudget = orderTotal > USDbalance;
 
   const orderResponse = () => {
-    if (orderDetail?.httpStatus > '201') {
+    if (!orderDetail?.httpStatus === 200) {
       setOrderFail(true);
       return (
         <p>
